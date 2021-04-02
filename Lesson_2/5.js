8a46b42b4737efd587a28e868d78670004386330
LucaGatfonova <70802387+LucaGatfonova@users.noreply.github.com>
2021-04-03 00:53:40 +0500

    Update 5.js

--- Lesson_2/5.js
+++ Lesson_2/5.js
@@ -1,19 +1,15 @@
 function add(a, b) {
-    return ('Сложение: ' + (a + b));
+    return (a + b);
 }
 
 function subtract(a, b) {
-    return ('Вычитание: ' + (a - b));
+    return (a - b);
 }
 
 function div (a, b) {
-    return ('Деление: ' + a / b);
+    return (a / b);
 }
 
 function mult (a, b) {
-    return ('Умножение: ' + a * b);
+    return (a * b);
 }
-console.log(add(10, 3))
-console.log(subtract(10, 3))
-console.log(div(10, 3))
-console.log(mult(10, 3))
